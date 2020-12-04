933fd236141599c627cb0e3eb350d9ba97dbead7
Kathy Bui <kathy.bui1992@gmail.com>
2020-12-03 22:36:13 -0700

    Create tests for code

--- tests/3_toboggan_trajectory_test.rb
+++ tests/3_toboggan_trajectory_test.rb
@@ -6,9 +6,28 @@ class TobogganTrajectoryTest < MiniTest::Test
 
   def setup
     @tt = TobogganTrajectory.new
+    @example_gird = {
+      0 => '..##.......',
+      1 => '#...#...#..',
+      2 => '.#....#..#.',
+      3 => '..#.#...#.#',
+      4 => '.#...##..#.',
+      5 => '..#.##.....',
+      6 => '.#.#.#....#',
+      7 => '.#........#',
+      8 => '#.##...#...',
+      9 => '#...##....#',
+      10 => '.#..#...#.#'
+    }
+    @grid = @tt.get_grid('./csv_files/3_toboggan_trajectory.csv')
   end
 
   def test_it_exists_with_attributes
     assert_instance_of TobogganTrajectory, @tt
+  end
+
+  def test_number_of_trees_hit
+    assert_equal 7, @tt.trees_hit(@example_grid, 3, 1)
+    # assert_equal 7, @tt.trees_hit(@grid, 3, 1)
   end
 end
